c625dcbdcffe508c6d81c1500c674a5f365d8f16
OJailson17 <JaylsonO17@gmail.com>
2022-07-18 20:19:57 -0300

    add create deliveryman feat

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { AuthenticateClientController } from './modules/account/useCases/authenticateClient/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const routes = Router();
 
 const authenticateClientController = new AuthenticateClientController();
 const createClientController = new CreateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
-routes.post('/auth/', authenticateClientController.handle);
-routes.post('/clients/', createClientController.handle);
+routes.post('/auth', authenticateClientController.handle);
+routes.post('/clients', createClientController.handle);
+routes.post('/deliveryman', createDeliverymanController.handle);
 
 export { routes };
