a387398cd402b0c4a62003cf0637d79db5ec9f74
OJailson17 <JaylsonO17@gmail.com>
2022-07-19 21:39:58 -0300

    add create delivery feat

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import { ensureAuthenticateClient } from './middleware/ensureAuthenticateClient';
 import { AuthenticateClientController } from './modules/account/useCases/authenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const routes = Router();
@@ -11,10 +13,16 @@ const authenticateDeliverymanCOntroller =
 	new AuthenticateDeliverymanController();
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post('/client/auth', authenticateClientController.handle);
 routes.post('/deliveryman/auth', authenticateDeliverymanCOntroller.handle);
 routes.post('/clients', createClientController.handle);
 routes.post('/deliveryman', createDeliverymanController.handle);
+routes.post(
+	'/delivery',
+	ensureAuthenticateClient,
+	createDeliveryController.handle,
+);
 
 export { routes };
