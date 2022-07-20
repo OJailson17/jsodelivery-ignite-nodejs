f9109bf54a2d5f8fce1257d79bc2afe9c934d844
OJailson17 <JaylsonO17@gmail.com>
2022-07-19 21:41:05 -0300

    alter tables

---
+++ prisma/migrations/20220719235043_alter_table_deliveries/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `clientsId` on the `deliveries` table. All the data in the column will be lost.
+  - You are about to drop the column `deliverymanId` on the `deliveries` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "deliveries" DROP COLUMN "clientsId",
+DROP COLUMN "deliverymanId";
