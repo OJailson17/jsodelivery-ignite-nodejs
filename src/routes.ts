fd45ae8a07aec24d7e5996025b4a229383326a54
OJailson17 <JaylsonO17@gmail.com>
2022-07-20 21:47:30 -0300

    add find all deliveries route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { ensureAuthenticateClient } from './middleware/ensureAuthenticateClient';
+import { ensureAuthenticateDeliveryman } from './middleware/ensureAuthenticateDeliveryman';
 import { AuthenticateClientController } from './modules/account/useCases/authenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
+import { FindAllWithoutEndDateController } from './modules/deliveries/useCases/findAllWithoutEndDate/FindAllWithoutEndDateController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const routes = Router();
@@ -11,9 +13,11 @@ const routes = Router();
 const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanCOntroller =
 	new AuthenticateDeliverymanController();
+
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const createDeliveryController = new CreateDeliveryController();
+const findAllWithoutEndDateController = new FindAllWithoutEndDateController();
 
 routes.post('/client/auth', authenticateClientController.handle);
 routes.post('/deliveryman/auth', authenticateDeliverymanCOntroller.handle);
@@ -23,6 +27,11 @@ routes.post(
 	'/delivery',
 	ensureAuthenticateClient,
 	createDeliveryController.handle,
+);
+routes.get(
+	'/delivery/available',
+	ensureAuthenticateDeliveryman,
+	findAllWithoutEndDateController.handle,
 );
 
 export { routes };
