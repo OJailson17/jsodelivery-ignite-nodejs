481304b5bd0c70c1f6366b827f76e9f67a79aef0
OJailson17 <JaylsonO17@gmail.com>
2022-07-20 22:29:56 -0300

    add update deliveryman route

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { AuthenticateDeliverymanController } from './modules/account/useCases/au
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { FindAllWithoutEndDateController } from './modules/deliveries/useCases/findAllWithoutEndDate/FindAllWithoutEndDateController';
+import { UpdateDeliverymanController } from './modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const routes = Router();
@@ -13,11 +14,11 @@ const routes = Router();
 const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanCOntroller =
 	new AuthenticateDeliverymanController();
-
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const createDeliveryController = new CreateDeliveryController();
 const findAllWithoutEndDateController = new FindAllWithoutEndDateController();
+const updateDeliverymanController = new UpdateDeliverymanController();
 
 routes.post('/client/auth', authenticateClientController.handle);
 routes.post('/deliveryman/auth', authenticateDeliverymanCOntroller.handle);
@@ -32,6 +33,11 @@ routes.get(
 	'/delivery/available',
 	ensureAuthenticateDeliveryman,
 	findAllWithoutEndDateController.handle,
+);
+routes.put(
+	'/delivery/updateDeliveryman/:id',
+	ensureAuthenticateDeliveryman,
+	updateDeliverymanController.handle,
 );
 
 export { routes };
